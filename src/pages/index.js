16032fec72596230f8288b423d457d85e5bea614
RomAlex1707 <121452122+RomAlex1707@users.noreply.github.com>
2023-05-24 14:54:21 -0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
 import "./index.css";
 import {
-  initialCards,
   buttonOpenPopupCard,
   enableValidationConfig,
   profileEditButton,
   popupProfileEdit,
   formAddCard,
   profileNameInput,
-  profileJobInput
+  profileJobInput,
+  avatar,
+  formEditAvatar,
+  loaderImage
 
 } from "../constants.js";
 
+import Api from "../components/Api.js";
 import Section from "../components/Section.js";
 import Card from "../components/Card.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithConfirmation from "../components/PopupWithConfirmation.js";
 import UserInfo from "../components/UserInfo.js";
 import FormValidator from "../components/FormValidator.js";
+
+
+//создаем экземпляр класса Api
+const api = new Api(
+  "https://mesto.nomoreparties.co/v1/cohort-66",
+  "dc4cc52d-8535-4ff0-845c-06f4815ac82c"
+);
 
 
 //создаем экземпляр класса попапа картинки
@@ -24,16 +35,55 @@ import FormValidator from "../components/FormValidator.js";
 const popupWithImage = new PopupWithImage('.popup_open-image');
 popupWithImage.setEventListeners();
 
-//создаем экземпляр класса Section, Card
 
-const cardGallery = new Section({
-    items: initialCards, 
-    renderer: (item) => {
-    addPicture(item);
+
+
+
+function addNewCard(card) {
+  const newCard = new Card(
+    card,
+    '.card__template',
+    () => {
+      popupWithImage.open(card.name, card.link);
+    },
+    () => {
+      popupWithFormDeleteCard.open(card._id, newCard);
+    },
+    (cardId, isLike) => {
+      return toggleLike(cardId, isLike, newCard);
+    },
+    card,
+    api.userId
+  );
+  return newCard;
+}
+
+const defaultGallery = new Section(
+  {
+    items: [],
+    renderer: (card) => {
+      defaultGallery.addItem(addNewCard(card).generateCard(card.likes.length));
+    },
   },
-}, '.elements__list');
+  ".elements__list"
+);
 
-cardGallery.renderItems();
+function loadGallery() {
+  avatar.style.backgroundImage = `url(${loaderImage})`;
+  Promise.all([api.getCards(), api.getUserInfo()])
+    .then((result) => {
+      defaultGallery.setItems(result[0].reverse());
+      defaultGallery.renderItems();
+      userInfo.setUserInfo({ name: result[1].name, profession: result[1].about });
+      userInfo.setAvatar(result[1].avatar);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+loadGallery();
+
+
 
 //вешаем листенер на кнопку открытия попапа карточек
 buttonOpenPopupCard.addEventListener('click', openAddCardPopup);
@@ -44,33 +94,38 @@ const validationFormAddCard = new FormValidator(enableValidationConfig, formAddC
 validationFormAddCard.enableValidation();
 
 
-function createCardInstance(item) {
-  return new Card(item, ".card__template", () => {
-    popupWithImage.open(item.name, item.link);
-  });
-}
-
-//функция загрузки картинки
-function addPicture(item) {
-  const newCard = createCardInstance(item).generateCard();
-  cardGallery.addItem(newCard);
-}
-
 //создаем экземпляр класса попапа формы добавления карточек и вешаем листенер
 const popupWithFormAddCard = new PopupWithForm({
   selector: '.popup_cards',
-  handleFormSubmit: addPicture,
+  handleFormSubmit: uploadPicture,
 });
 popupWithFormAddCard.setEventListeners();
+
+
+function uploadPicture(card) {
+  return new Promise((resolve, reject) => {
+    api
+      .addCard(card)
+      .then((card) => {
+        defaultGallery.addItem(
+          addNewCard(card).generateCard(card.likes.length)
+        );
+        resolve();
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
+}
 
 
 //функция открытия картинок
 function openAddCardPopup() {
   validationFormAddCard.hideAllInputErrors();
-  validationFormAddCard._toggleButtonState();
-
   popupWithFormAddCard.open();
 }
+
 
 
 //создаем экземпляр класса валидации попапа редактирования профиля
@@ -87,19 +142,134 @@ const popupWithFormEditProfile = new PopupWithForm({
 });
 popupWithFormEditProfile.setEventListeners();
 
+
+
 // создание экземпляра класса, отвечающего за отображение информации о пользователе
 const userInfo = new UserInfo ({
   username: '.profile__name',
   job: '.profile__description',
+  avatar: ".profile__avatar",
 });
 
+function submitEditProfileForm(item) {
+  return api
+    .editProfile(item)
+    .then(() => {
+      loadUserInfo();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-function submitEditProfileForm(data) {
-  userInfo.setUserInfo({
-    name: data.name,
-    profession: data.profession
+
+
+//функция вставки данных юзера с сервера на страницу профиля
+function loadUserInfo(){
+  api.getUserInfo()
+  .then(result => {
+    userInfo.setUserInfo({name:result.name, profession:result.about})
+  })
+  .catch(err => console.log(`Ошибка: ${err}`))
+}
+
+//функция вставки аватара с сервера на страницу
+function loadAvatar() {
+  avatar.style.backgroundImage = `url(${loaderImage})`;
+  api
+    .getUserInfo()
+    .then((result) => {
+      userInfo.setAvatar(result.avatar);
+    })
+    .catch((error) => console.log(error));
+}
+
+//форма редактирования аватара
+avatar.addEventListener('click', openAvatarPopup);
+
+const validationFormEditAvatar = new FormValidator(enableValidationConfig, formEditAvatar);
+validationFormEditAvatar.enableValidation();
+
+const popupWithFormEditAvatar = new PopupWithForm({
+  selector: '.popup_edit-avatar',
+  handleFormSubmit: editAvatar,
+});
+popupWithFormEditAvatar.setEventListeners();
+
+function editAvatar(item) {
+  return new Promise((resolve, reject) => {
+    api
+      .editAvatar(item)
+      .then(() => {
+        loadAvatar();
+        resolve();
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
   });
 }
+
+function openAvatarPopup() {
+  formEditAvatar.link.value = avatar.style.backgroundImage.slice(5, -2);
+  validationFormEditAvatar.hideAllInputErrors();
+  popupWithFormEditAvatar.open();
+}
+
+//создание экземпляра класса попапа подтверждения удаления
+const popupWithFormDeleteCard = new PopupWithConfirmation({
+  selector: '.popup_delete-card',
+  handleFormSubmit: deleteCard
+});
+popupWithFormDeleteCard.setEventListeners();
+
+//функция удаления карточки
+function deleteCard(idCard, cardInstance) {
+  return new Promise((resolve, reject) => {
+    api
+      .deleteCard(idCard)
+      .then(() => {
+        cardInstance.deleteImage();
+        resolve();
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
+}
+//функция установки или удаления лайка
+function toggleLike(cardId, isLike, cardInstance) {
+  if (isLike) {
+    return new Promise((resolve, reject) => {
+      api
+        .addLike(cardId)
+        .then((result) => {
+          cardInstance.loadLike(result);
+          resolve();
+        })
+        .catch((error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      api
+        .deleteLike(cardId)
+        .then((result) => {
+          cardInstance.loadLike(result);
+          resolve();
+        })
+        .catch((error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
+  }
+}
+
 
 //функция открытия попапа редактирования профиля
 
@@ -125,5 +295,3 @@ profileEditButton.addEventListener('click', () => {
   });
  
 });
-
-
